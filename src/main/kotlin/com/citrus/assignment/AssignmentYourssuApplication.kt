5088c50d504242a2a6b44bec314d23cb550c9587
citrusinesis <jihojiho2003@gmail.com>
2022-10-03 02:10:14 +0900

    initialize

---
+++ src/main/kotlin/com/citrus/assignment/AssignmentYourssuApplication.kt
@@ -0,0 +1,11 @@
+package com.citrus.assignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssignmentYourssuApplication
+
+fun main(args: Array<String>) {
+    runApplication<AssignmentYourssuApplication>(*args)
+}
---
+++ src/test/kotlin/com/citrus/assignment/AssignmentYourssuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.citrus.assignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssignmentYourssuApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
