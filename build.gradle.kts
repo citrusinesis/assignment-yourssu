feccb1ff0684f16ea3fc0a091f0dccd5fe2e7ca0
citrusinesis <jihojiho2003@gmail.com>
2022-10-04 17:18:06 +0900

    Dev: Enable Auto Reload

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-devtools:2.7.3")
     runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
