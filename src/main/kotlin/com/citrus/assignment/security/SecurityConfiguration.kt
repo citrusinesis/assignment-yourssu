79590f8680bf74f0b9631fc7f8d7170aada74ef5
citrusinesis <jihojiho2003@gmail.com>
2022-11-08 14:05:26 +0900

    Refactor: Java Style config to Kotlin DSL

--- src/main/kotlin/com/citrus/assignment/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/citrus/assignment/security/SecurityConfiguration.kt
@@ -5,27 +5,29 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfiguration(
     @Autowired private val jwtUtils: JwtUtils,
 ) {
-    private val filteredURL: Array<String> = arrayOf("/article/**", "/comment/**")
-
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.cors().disable()
-            .csrf().disable()
-            .formLogin().disable()
-            .httpBasic().disable()
-            .headers().frameOptions().disable()
+        http {
+            cors { disable() }
+            csrf { disable() }
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtFilter(jwtUtils))
+            httpBasic { disable() }
+            headers { frameOptions { disable() } }
+        }
         return http.build()
     }
 }
