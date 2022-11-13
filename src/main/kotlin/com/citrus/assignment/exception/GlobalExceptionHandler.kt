688cd3f81966d8ebf97a29a271380c55cb04c61e
citrusinesis <jihojiho2003@gmail.com>
2022-11-14 02:20:23 +0900

    Add: GlobalExceptionHandler.kt handlingException

--- src/main/kotlin/com/citrus/assignment/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/citrus/assignment/exception/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package com.citrus.assignment.exception
 
 import com.citrus.assignment.transfer.ErrorResponse
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest
 @RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(value = [CustomException::class])
-    fun handlingException(
+    fun handlingCustomException(
         ex: CustomException,
         request: HttpServletRequest
     ): ResponseEntity<ErrorResponse> {
@@ -22,5 +23,20 @@ class GlobalExceptionHandler {
             requestURI = request.requestURI
         )
         return ResponseEntity(errorResponse, errorCode.status)
+    }
+
+    @ExceptionHandler(value = [Exception::class, java.lang.Exception::class])
+    fun handlingException(
+        ex: Exception,
+        request: HttpServletRequest
+    ): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(
+            ErrorResponse(
+                time = LocalDateTime.now(),
+                status = ex.cause.toString(),
+                message = ex.message ?: "Internal Server Error",
+                requestURI = request.requestURI
+            ), HttpStatus.INTERNAL_SERVER_ERROR
+        )
     }
 }
