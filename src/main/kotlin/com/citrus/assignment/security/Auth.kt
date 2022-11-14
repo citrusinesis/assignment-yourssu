d37c884b4541f7d8deb0ae6db27b99f68dce521c
citrusinesis <jihojiho2003@gmail.com>
2022-11-14 20:12:13 +0900

    Add: Implement AuthHandlerMethodArgumentResolver, Auth

---
+++ src/main/kotlin/com/citrus/assignment/security/Auth.kt
@@ -0,0 +1,6 @@
+package com.citrus.assignment.security
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Auth()
+
---
+++ src/main/kotlin/com/citrus/assignment/security/AuthHandlerMethodArgumentResolver.kt
@@ -0,0 +1,32 @@
+package com.citrus.assignment.security
+
+import com.citrus.assignment.exception.CustomException
+import com.citrus.assignment.exception.ErrorCode
+import com.citrus.assignment.transfer.auth.AuthInfo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+@Component
+class AuthHandlerMethodArgumentResolver(
+    @Autowired val jwtUtils: JwtUtils,
+): HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean =
+        parameter.parameterType.isAssignableFrom(AuthInfo::class.java)
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): Any? = AuthInfo(
+        email = jwtUtils.getIdentifier(
+            webRequest.getHeader("Authorization")
+                ?: throw CustomException(ErrorCode.GET_HEADER_CONFLICT)
+        )
+    )
+}
